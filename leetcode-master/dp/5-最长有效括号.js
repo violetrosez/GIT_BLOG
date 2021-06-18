5d60caf44b56e50b50647453045e0f5939ed02f0
violetrosez <violetrosezq@gmail.com>
2021-06-18 18:19:08 +0800

    note

--- leetcode-master/dp/5-最长有效括号.js
+++ leetcode-master/dp/5-最长有效括号.js
@@ -2,15 +2,21 @@
 // 输入：s = ")()())"
 // 输出：4
 // 解释：最长有效括号子串是 "()()"
+
+/**
+ * 思路： f[i] 表示以s[i]为结尾的子串中，形成的最长有效子串的长度，且有效子串是以s[i]为结尾。
+ * @param {*} str
+ * @returns
+ */
 function maxLen(str) {
-  let f = [];
+  let f = new Array(str.length).fill(0);
   let max = 0;
   for (let index = 0; index < str.length; index++) {
     if (str[index] == ")") {
       // ...()的情况
       if (str[index - 1] == "(") {
         f[index] = index > 2 ? f[index - 2] + 2 : 2;
-      } else if (str[index - 1] == ")") {
+      } else if (str[index - f[index - 1] - 1] == "(") {
         // console.log(f[index - f[index - 1] - 2]);
         f[index] =
           f[index - 1] +
@@ -18,12 +24,10 @@ function maxLen(str) {
           2;
       }
       max = Math.max(max, f[index]);
-    } else {
-      f[index] = 0;
     }
   }
-  console.log(f);
+  //   console.log(f);
   return max;
 }
 
-console.log(maxLen("()(())"));
+console.log(maxLen("(())"));
